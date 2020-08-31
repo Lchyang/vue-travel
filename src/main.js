5731f0b54249fe2cd0b21fae6c63c90781caf672
lichy <lichunyangcoder@gmail.com>
2020-08-31 23:13:35 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import fastClick from 'fastclick'
+// 多种终端页面重置效果统一
+import './assets/styles/reset.css'
+// 多倍屛物理边框不是1px的问题
+import './assets/styles/border.css'
+
+fastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
