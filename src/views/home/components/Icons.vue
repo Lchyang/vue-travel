764f5a6f610f6faaa1bc7ff99217708ae7c3d6b3
lichy <lichunyangcoder@gmail.com>
2020-09-04 01:35:30 +0800

    add icons img

---
+++ src/views/home/components/Icons.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="icons-swapper">
+    <div class="icons">
+      <div class="img-swapper">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" />
+      </div>
+      <div class="img-text">热门景区</div>
+    </div>
+    <div class="icons">
+      <div class="img-swapper">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" />
+      </div>
+      <div class="img-text">热门景区</div>
+    </div>
+    <div class="icons">
+      <div class="img-swapper">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" />
+      </div>
+      <div class="img-text">热门景区</div>
+    </div>
+    <div class="icons">
+      <div class="img-swapper">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" />
+      </div>
+      <div class="img-text">热门景区</div>
+    </div>
+    <div class="icons">
+      <div class="img-swapper">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" />
+      </div>
+      <div class="img-text">热门景区</div>
+    </div>
+    <div class="icons">
+      <div class="img-swapper">
+      <img class="icon-img" src="http://img1.qunarzz.com/piao/fusion/1611/54/ace00878a52d9702.png" />
+      </div>
+      <div class="img-text">热门景区</div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="stylus" scoped>
+.icons-swapper
+  width: 100%
+  height: 0
+  padding-bottom: 50%
+  .icons
+    position relative
+    float: left
+    width: 25%
+    height: 0
+    padding-bottom: 25%
+    .img-swapper
+      position absolute
+      top: 0
+      left: 0
+      right: 0
+      bottom: .44rem
+      box-sizing: border-box
+      padding: .1rem
+      .icon-img
+        display: block
+        margin: 0 auto
+        height: 100%
+    .img-text
+      position: absolute
+      left: 0
+      right: 0
+      bottom: 0
+      color: #ccc
+      text-align : center
+      line-height: .44rem
+</style>
