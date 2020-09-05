5e530227a6cc0826a9a22fb90571d88bd53d1338
lichy <lichunyangcoder@gmail.com>
2020-09-06 00:24:25 +0800

    city-router

---
+++ src/views/city/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="city-header">
+      <div class="iconfont-swaper">
+        <router-link to="/">
+          <span class="iconfont back-icon">&#xe611;</span>
+        </router-link>
+      </div>城市选择
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles'
+
+.city-header
+  height: $headerHeight
+  background: $bgColor
+  line-height: $headerHeight
+  text-align: center
+  font-size: 0.32rem
+  color: #fff
+  .iconfont-swaper
+    position: absolute
+    left: 0.2rem
+    top: 0
+    .back-icon
+      font-size: 0.32rem
+      color: #fff
+</style>
--- src/views/home/components/Header.vue
+++ src/views/home/components/Header.vue
@@ -8,10 +8,12 @@
       输入城市/景点/游玩主题
     </div>
     <div class="header-city">
-      <div class="header-right">
-        城市
-        <span class="iconfont arrow-icon">&#xe6aa;</span>
-      </div>
+      <router-link to="/city">
+        <div class="header-right">
+          城市
+          <span class="iconfont arrow-icon">&#xe6aa;</span>
+        </div>
+      </router-link>
     </div>
   </div>
 </template>
@@ -21,6 +23,7 @@ export default {
 </script>
 <style lang="stylus" scoped>
 @import '~styles/varibles'
+
 .header
   display: flex
   height: $headerHeight
@@ -45,6 +48,8 @@ export default {
     width: 1.2rem
     line-height: 0.86rem
     text-align: center
+    .header-right
+      color: #fff
     .arrow-icon
       margin-left: -0.1rem
 </style>
