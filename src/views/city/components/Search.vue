47e3166a49149c58e483f6020e7f9790c71c0f67
lichy <lichunyangcoder@gmail.com>
2020-09-06 08:59:20 +0800

    city search

---
+++ src/views/city/components/Search.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="search">
+    <input class="input" placeholder="请输入要搜索的城市"/>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="stylus">
+@import '~styles/varibles'
+
+.search
+  height: 0.72rem
+  background: $bgColor
+  padding: 0 0.1rem
+  .input
+    box-sizing: border-box
+    height: 0.62rem
+    line-height: 0.62rem
+    text-align: center
+    width: 100%
+    border-radius: 0.06rem
+    padding: 0 .1rem
+</style>
